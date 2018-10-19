51bf57289ffd8cb8ff49033b1e32ae9c5ff85a23
Milan Petrović <millllan@gmail.com>
2018-10-19 23:17:53 +0200

    npm update

--- src/app/components/account-logo/account-logo.module.ts
+++ src/app/components/account-logo/account-logo.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { AccountLogoComponent } from './account-logo.component';
 
 @NgModule({
-	declarations: [
-        	AccountLogoComponent,
-	],
-	entryComponents: [],
-	imports: [],
-	exports: [
-        	AccountLogoComponent
-	]
+    declarations: [
+        AccountLogoComponent,
+    ],
+    entryComponents: [],
+    imports: [],
+    exports: [
+        AccountLogoComponent
+    ]
 })
 export class AccountLogoComponentModule {}
